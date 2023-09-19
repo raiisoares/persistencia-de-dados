28b48d5f2a1dbd84a0fa4c9006940593ecb9f17b
Raí Soares <raisoares.dev@gmail.com>
2023-09-19 20:21:30 -0300

    Create and remove books added

--- src/bookInterface.rb
+++ src/bookInterface.rb
@@ -1,7 +1,6 @@
 require_relative 'book'
 
 class BookInterface
-
   def create_book(file)
     print "id: "
     id = gets.chomp.to_i
@@ -18,7 +17,34 @@ class BookInterface
     file.puts("#{book.id}|#{book.author}|#{book.title}|#{book.genre}|#{book.year}")
     puts "Livro cadastrado com sucesso!"
   end
+
+  def remover_book_by_id(file)
+    print "Informe o id do livro que será removido: "
+    id = gets.chomp.to_i
+
+    lines = file.readlines
+    file.rewind
+    file.truncate(0)
+
+    book_removed = false
+    
+    lines.each do |line|
+      book_id, _, _, _, _ = line.chomp.split('|')
+      if book_id.to_i != id
+        file.puts(line)
+      else
+        book_removed = true
+      end
+    end
+
+    if book_removed
+      puts "\nLivro removido com sucesso!\n"
+    else
+      puts "\nLivro não encontrado!\n"
+    end
+  end
 end
+          
 
 
 
--- src/main.rb
+++ src/main.rb
@@ -2,7 +2,7 @@ require_relative "bookInterface"
 
 def create_file
   if File.exist?('books.txt')
-    File.open('books.txt', 'r+')
+    File.open('books.txt', 'a+')
   else
     File.new('books.txt', 'w+')
   end
@@ -11,7 +11,8 @@ end
  interface = BookInterface.new
  file = create_file
 
- interface.create_book(file)
+#  interface.create_book(file)
+ interface.remover_book_by_id(file)
 
 
 
