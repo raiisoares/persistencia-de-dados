f79fce49c3bc5afda1bbaf8460b7499d4ed09e83
RaulMiguelAS <raul.2021104876@unicap.br>
2023-09-24 10:43:41 -0300

    Começo parte interativa

--- src/main.rb
+++ src/main.rb
@@ -2,15 +2,35 @@ require 'json'
 require_relative "bookInterface"
 
 #TODO: Interface para aplicação
+
 def main
   controller = BookInterface.new
   file = create_file
+  puts "Opções:"
+  puts "1 - Adicionar livro"
+  puts "2 - Remover livro"
+  puts "3 - Listar livros"
+  puts "4 - Procurar livro"
+  puts "5 - Atualizar livros"
+  puts "0 - Cancelar"
+  print "Escolha o que deseja fazer "
+  option = gets.chomp.to_i
 
-  #  controller.create_book(file)
-  #  controller.remove_book_by_id(file)
-  #  controller.index_books(file)
-  #  controller.show_book(file)
-  # controller.update_book(file)
+  case option
+  when 1
+    controller.create_book(file)
+  when 2
+    controller.remove_book_by_id(file)
+  when 3
+    controller.index_books(file)
+  when 4
+    controller.show_book(file)
+  when 5
+    controller.update_book(file)
+  when 0
+    puts 
+  end
+  
 end
 
 def create_file
